67235df246f0fa3436921ec19e6352d53b1a9fbd
Trung297 <trung297005@gmail.com>
2024-10-16 21:09:39 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public abstract class Person
+    {
+        private string Name { get; }
+        private string Email { get; }
+        private DateTime DateOfBirth { get; }
+        public string Phone_num { get; set; }
+        public string Address { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - DateOfBirth.Year;
+            }
+        }
+
+        protected Person(string name, string email, DateTime dateofbirth, string phone_num, string address)
+        {
+            Name = name;
+            Email = email;
+            DateOfBirth = dateofbirth;
+            Phone_num = phone_num;
+            Address = address;
+        }
+
+    }
+}
---
+++ Report.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp21
+{
+    public class Report
+    {
+        public int ReportID { get; set; }
+        public int EmployeeID { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public int ValidAbsentDays { get; private set; }
+        public int InvalidAbsentDays { get; private set; }
+        public int OvertimeDays { get; private set; }
+        public int LateTimes { get; private set; }
+        public int AttendanceDays { get; private set; }
+        public TimeSpan OvertimeHours { get; private set; }
+
+        // Thêm List để lưu trữ tất cả các báo cáo
+        public static List<Report> AllReports { get; set; } = new List<Report>();
+
+        public Report(int reportID, int employeeID, DateTime date)
+        {
+            ReportID = reportID;
+            EmployeeID = employeeID;
+            Date = date;
+            ValidAbsentDays = 12;
+            InvalidAbsentDays = 0;
+            OvertimeDays = 0;
+            LateTimes = 0;
+            AttendanceDays = 0;
+            // Thêm báo cáo mới vào List
+            AllReports.Add(this);
+            OvertimeHours = TimeSpan.Zero;
+        }
+        
+        public void ValidAbsent(Attendance attendance) // nghỉ có phép
+        {
+            if(!attendance.CheckInTime.HasValue && !attendance.CheckOutTime.HasValue)
+            {
+                if (ValidAbsentDays > 0)
+                {
+                    ValidAbsentDays--;
+                    Console.WriteLine($"Valid absent days remain:{ValidAbsentDays}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid absent days remain: 0");
+                }
+            }
+        }
+        public void AddInvalidAbsentDays() // nghỉ ko phép
+        {
+            int initialValidAbsentDays = ValidAbsentDays;
+
+            foreach (Attendance attendance in Attendances)
+            {
+                if(!attendance.CheckOutTime.HasValue && !attendance.CheckInTime.HasValue)
+                {
+                    ValidAbsent(attendance);
+
+                    if (ValidAbsentDays == 0)
+                    {
+                        InvalidAbsentDays++;
+                        Console.WriteLine($"invalid absent days: {InvalidAbsentDays}");
+                    }
+                }
+            }
+        }
+        public void AddOvertimeDays() // tăng ca
+        {
+            //nếu làm nhiều hơn giờ hành chính thì sẽ tăng OvertimeDays
+            OvertimeDays++;
+        }
+        public void AddLateTimes() // đi trễ
+        {
+            foreach (Attendance attendance in Attendances)
+            {
+                if (attendance.CheckOutTime.HasValue)
+                {
+                    LateTimes++;
+                }
+            }
+        }
+        public void CalculateAttendanceDay() // tính tổng ngày đi làm 
+        {
+            foreach (Attendance attendance in Attendances)
+            {
+                if (attendance.CheckInTime.HasValue || attendance.CheckOutTime.HasValue || ValidAbsentDays >=0 )
+                {
+                    AttendanceDays++;
+                }
+            }
+        }
+        public void DisplayReport()
+        {
+            Console.WriteLine($"Report ID: {ReportID}");
+            Console.WriteLine($"Employee ID: {EmployeeID}");
+            Console.WriteLine($"Date: {Date.ToShortDateString()}");
+            Console.WriteLine($"Valid Absent Days Remain: {ValidAbsentDays}");
+            Console.WriteLine($"Invalid Absent Days: {InvalidAbsentDays}");
+            Console.WriteLine($"Overtime Days: {OvertimeDays}");
+            Console.WriteLine($"Late Times: {LateTimes}");
+            Console.WriteLine($"Attendance Days: {AttendanceDays}");
+            Console.WriteLine($"Total Overtime Hours: {OvertimeHours}");
+            Console.WriteLine("----------------------------");
+        }
+
+    }
+    
+}
