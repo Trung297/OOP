e66814ffc61f4a38618427e9808c02e067dfe65d
Trung297 <trung297005@gmail.com>
2024-10-21 15:53:23 +0700

    Person.cs

--- Person.cs
+++ Person.cs
@@ -8,11 +8,11 @@ namespace ConsoleApp3
 {
     public abstract class Person
     {
-        private string Name { get; }
-        private string Email { get; }
-        private DateTime DateOfBirth { get; }
-        public string Phone_num { get; set; }
-        public string Address { get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set;}
+        public DateTime DateOfBirth { get; private set; }
+        public string Phone_num { get; private set; }
+        public string Address { get; private set; }
 
         public int Age
         {
